23c21846ff6bfb04bc79978bd8223214de0c8c35
Soroban66 <khft663@icloud.com>
2022-06-09 22:29:13 +0900

    Initial Commit

---
+++ timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  timer
+//
+//  Created by 藤城将志 on 2022/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by 藤城将志 on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
